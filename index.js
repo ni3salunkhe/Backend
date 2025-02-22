2d2c507b0d64193b4249640e4b93611ad426fa36
ni3 <salunkhe6661@gmail.com>
2025-02-22 17:07:28 +0530

    Logger file added to track error logs

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express"
 import 'dotenv/config'
+import logger from "./logger.js";
+import morgan from "morgan";
+
 
 const app = express()
 const port = process.env.PORT || 4000
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
 
 // app.get("/", (req, res) => {
 //     res.send("Hello From Nitin with nodemon")
@@ -22,26 +43,55 @@ const port = process.env.PORT || 4000
 
 app.use(express.json())
 
-let userData=[]
-let next=1
-app.post("/user",(req, res)=>{
-    const {name,price}=req.body
-    const newData={id:next++, name, price}
+let userData = []
+let next = 1
+
+// add new user 
+app.post("/user", (req, res) => {
+    logger.info("User added")
+    const { name, price } = req.body
+    const newData = { id: next++, name, price }
     userData.push(newData)
     res.status(201).send(newData)
 })
 
-app.get("/user",(req, res)=>{
+//get all users
+app.get("/user", (req, res) => {
     res.status(200).send(userData)
 })
 
-app.get("/user/:id",(req, res)=>{
+//get specific user on the basis of id
+app.get("/user/:id", (req, res) => {
     const user = userData.find(user => user.id === parseInt(req.params.id));
     if (!user) {
         return res.status(404).send("User not found");
     }
     res.status(200).send(user);
 })
+
+//update user
+app.put("/user/:id", (req, res) => {
+    const user = userData.find(user => user.id === parseInt(req.params.id))
+    if (!user) {
+        return res.status(404).send("User not found")
+    }
+    const { name, price } = req.body
+    user.name = name
+    user.price = price
+    res.status(200).send(user)
+})
+
+//delete user
+app.delete("/user/:id", (req, res) => {
+    const user = userData.findIndex(user => user.id === parseInt(req.params.id))
+    if (user === -1) {
+        return res.status(404).send("No user with this id exists")
+    }
+    userData.splice(user, 1)
+    return res.status(204).send("deleted")
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
